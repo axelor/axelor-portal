d65fec20b98e5271d994f2dd8a1da8d9f3c63e76
p.shah <p.shah@axelor.com>
2024-11-06 12:33:26 +0530

    [Feature #86409] [TaskComments] Redmine like comments for project task

--- src/main/java/com/axelor/apps/portal/module/PortalModule.java
+++ src/main/java/com/axelor/apps/portal/module/PortalModule.java
@@ -19,23 +19,32 @@
 package com.axelor.apps.portal.module;
 
 import com.axelor.app.AxelorModule;
+import com.axelor.apps.businesssupport.db.repo.ProjectTaskBusinessSupportRepository;
 import com.axelor.apps.portal.db.repo.DMSFilePortalRepository;
 import com.axelor.apps.portal.db.repo.MailMessagePortalRepository;
+import com.axelor.apps.portal.db.repo.ProjectTaskPortalRepository;
+import com.axelor.apps.portal.service.MailMessageFileService;
+import com.axelor.apps.portal.service.MailMessageFileServiceImpl;
+import com.axelor.apps.portal.service.MailMessagePortalService;
+import com.axelor.apps.portal.service.MailMessagePortalServiceImpl;
 import com.axelor.apps.portal.service.SaleOrderPortalService;
 import com.axelor.apps.portal.service.SaleOrderPortalServiceImpl;
 import com.axelor.apps.portal.service.StockMovePortalServiceImpl;
 import com.axelor.apps.production.service.StockMoveServiceProductionImpl;
-import com.axelor.apps.project.db.repo.MailMessageProjectRepository;
 import com.axelor.dms.db.repo.DMSFileRepository;
+import com.axelor.mail.db.repo.MailMessageRepository;
 
 public class PortalModule extends AxelorModule {
 
   @Override
   protected void configure() {
+
     bind(SaleOrderPortalService.class).to(SaleOrderPortalServiceImpl.class);
     bind(StockMoveServiceProductionImpl.class).to(StockMovePortalServiceImpl.class);
-
     bind(DMSFileRepository.class).to(DMSFilePortalRepository.class);
-    bind(MailMessageProjectRepository.class).to(MailMessagePortalRepository.class);
+    bind(MailMessageRepository.class).to(MailMessagePortalRepository.class);
+    bind(ProjectTaskBusinessSupportRepository.class).to(ProjectTaskPortalRepository.class);
+    bind(MailMessagePortalService.class).to(MailMessagePortalServiceImpl.class);
+    bind(MailMessageFileService.class).to(MailMessageFileServiceImpl.class);
   }
 }
