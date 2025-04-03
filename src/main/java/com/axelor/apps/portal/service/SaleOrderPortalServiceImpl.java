0041523fc0157969843b90e94c322b2ee3ff2793
Riddhi CHAUHAN <r.chauhan@axelor.com>
2025-04-03 14:28:32 +0530

    RM#93264 Backend: Create Order Webservice to Include Invoicing and Delivery Address

--- src/main/java/com/axelor/apps/portal/service/SaleOrderPortalServiceImpl.java
+++ src/main/java/com/axelor/apps/portal/service/SaleOrderPortalServiceImpl.java
@@ -22,9 +22,11 @@ import com.axelor.apps.account.db.AccountManagement;
 import com.axelor.apps.account.db.Tax;
 import com.axelor.apps.account.db.TaxEquiv;
 import com.axelor.apps.base.AxelorException;
+import com.axelor.apps.base.db.Address;
 import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.base.db.Product;
+import com.axelor.apps.base.db.repo.AddressRepository;
 import com.axelor.apps.base.db.repo.PartnerRepository;
 import com.axelor.apps.base.db.repo.PriceListRepository;
 import com.axelor.apps.base.db.repo.ProductRepository;
@@ -96,6 +98,7 @@ public class SaleOrderPortalServiceImpl implements SaleOrderPortalService {
   protected SaleOrderRepository saleOrderRepo;
   protected PortalWorkspaceRepository portalWorkspaceRepo;
   protected StockLocationRepository stockLocationRepository;
+  protected AddressRepository addressRepo;
 
   @Inject
   public SaleOrderPortalServiceImpl(
@@ -119,7 +122,8 @@ public class SaleOrderPortalServiceImpl implements SaleOrderPortalService {
       ProductRepository productRepo,
       SaleOrderRepository saleOrderRepo,
       PortalWorkspaceRepository portalWorkspaceRepo,
-      StockLocationRepository stockLocationRepository) {
+      StockLocationRepository stockLocationRepository,
+      AddressRepository addressRepo) {
     this.saleOrdeCreateService = saleOrdeCreateService;
     this.saleOrderLinePricingService = saleOrderLinePricingService;
     this.saleOrderLinePriceService = saleOrderLinePriceService;
@@ -141,6 +145,7 @@ public class SaleOrderPortalServiceImpl implements SaleOrderPortalService {
     this.saleOrderRepo = saleOrderRepo;
     this.portalWorkspaceRepo = portalWorkspaceRepo;
     this.stockLocationRepository = stockLocationRepository;
+    this.addressRepo = addressRepo;
   }
 
   @Override
@@ -236,8 +241,23 @@ public class SaleOrderPortalServiceImpl implements SaleOrderPortalService {
         portalWorkspace =
             portalWorkspaceRepo.find(Long.parseLong(values.get("workspaceId").toString()));
       }
-      values.get("workspaceId");
       saleOrder.setPortalWorkspace(portalWorkspace);
+
+      Address deliveryAddress = null;
+      if (values.containsKey("deliveryAddressId")
+          && ObjectUtils.notEmpty(values.get("deliveryAddressId"))) {
+        deliveryAddress =
+            addressRepo.find(Long.parseLong(values.get("deliveryAddressId").toString()));
+      }
+      saleOrder.setDeliveryAddress(deliveryAddress);
+
+      Address invocingAddress = null;
+      if (values.containsKey("invocingAddressId")
+          && ObjectUtils.notEmpty(values.get("invocingAddressId"))) {
+        invocingAddress =
+            addressRepo.find(Long.parseLong(values.get("invocingAddressId").toString()));
+      }
+      saleOrder.setMainInvoicingAddress(invocingAddress);
     }
 
     return saleOrder;
